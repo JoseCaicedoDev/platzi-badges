eb7d31b88037f90f50290347eb38d676d2ef3149
Jose Caicedo <jose.caicedo@outlook.com>
2020-12-19 22:55:43 -0500

    Updates

---
+++ precache-manifest.b20ecf517a7f90fcb2cb2c503e509cee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "d751e04e7aa8c6f732e3",
+    "url": "/platzi-badges/static/css/main.e05d995c.chunk.css"
+  },
+  {
+    "revision": "d751e04e7aa8c6f732e3",
+    "url": "/platzi-badges/static/js/main.d751e04e.chunk.js"
+  },
+  {
+    "revision": "0f7117b146aebd482afe",
+    "url": "/platzi-badges/static/js/runtime~main.0f7117b1.js"
+  },
+  {
+    "revision": "f1fcf8a55e2fc3ab008c",
+    "url": "/platzi-badges/static/css/2.c8df3ba7.chunk.css"
+  },
+  {
+    "revision": "f1fcf8a55e2fc3ab008c",
+    "url": "/platzi-badges/static/js/2.f1fcf8a5.chunk.js"
+  },
+  {
+    "revision": "038da6d9a7472b0c7b885a3aa9dea93e",
+    "url": "/platzi-badges/static/media/JC.038da6d9.png"
+  },
+  {
+    "revision": "84e145c6d1ae2224e38539f1a56d8fbf",
+    "url": "/platzi-badges/static/media/notError.84e145c6.png"
+  },
+  {
+    "revision": "0abcdfcd1e323649a0338a204fe8b853",
+    "url": "/platzi-badges/static/media/stars.0abcdfcd.svg"
+  },
+  {
+    "revision": "210b1ff123239219e2d0ecffd1b641be",
+    "url": "/platzi-badges/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/badges/precache-manifest.e6adae795eeb09ac00590b87f44a527c.js"
+  "/platzi-badges/precache-manifest.b20ecf517a7f90fcb2cb2c503e509cee.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/badges/index.html", {
+workbox.routing.registerNavigationRoute("/platzi-badges/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
